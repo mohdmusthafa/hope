e9399c8971cb76370261ad127e9815f462b2ad09
Mohammed Musthafa <absentauthor21@gmail.com>
2021-11-06 06:05:05 +0000

    contacts repo crud

--- packages/api/repository/contacts.repository.js
+++ packages/api/repository/contacts.repository.js
@@ -9,6 +9,40 @@ const getSocialWorkers = async (centre_id) => {
     return result;
 }
 
+const addSocialWorker = async (name, designation, contact_no, address, centre_id) => {
+    const request = {
+        name,
+        designation,
+        contact_no,
+        address,
+        centreId: centre_id
+    }
+
+    const result = await models.social_workers.create(request)
+    return result;
+}
+
+const updateSocialWorker = async (id, name, designation, contact_no, address, centre_id) => {
+    const request = {
+        name,
+        designation,
+        contact_no,
+        address,
+        centreId: centre_id
+    }
+    
+    const result = await models.social_workers.update(request, { where: { id: id }});
+    return result;
+}
+
+const deleteSocialWorker = async (id) => {
+    const result = await models.social_workers.destroy({ where: { id: id }})
+    return result;
+}
+
 module.exports = {
-    getSocialWorkers
+    getSocialWorkers,
+    addSocialWorker,
+    updateSocialWorker,
+    deleteSocialWorker
 }
--- packages/api/routes/contacts.router.js
+++ packages/api/routes/contacts.router.js
@@ -4,11 +4,43 @@ const common_query = require('../middlewares/common_query');
 
 const router = express.Router();
 
+
+router.delete('/', async (req, res) => {
+    const { id } = req.query;
+    await contacts_repo.deleteSocialWorker(id);
+    res.status(200).json({ message: `${id} deleted successfully!`});
+})
+
+router.put('/', async (req, res) => {
+    const { id } = req.query;
+    const { name, designation, contact_no, address } = req.body;
+
+    if(!name && !designation && !contact_no && !address) {
+        return res.status(400).json({ message: 'Please provide name, designation, contact no and address'})
+    }
+
+    const result = await contacts_repo.updateSocialWorker(id, name, designation, contact_no, address, req.centre_id);
+    res.status(200).json({ message: `${name} updated successfully!`})
+})
+
+
 router.use(common_query);
 
 router.get('/', async (req, res) => {
     const social_workers = await contacts_repo.getSocialWorkers(req.centre_id);
     res.status(200).json(social_workers)
 })
+
+router.post('/', async(req, res) => {
+    const { name, designation, contact_no, address } = req.body;
+
+    if(!name && !designation && !contact_no && !address) {
+        return res.status(400).json({ message: 'Please provide name, designation, contact no and address'})
+    }
+
+    const result = await contacts_repo.addSocialWorker(name, designation, contact_no, address, req.centre_id);
+    res.status(201).json({ message: `${name} added successfully!`})
+})
+
 
 module.exports = router;
